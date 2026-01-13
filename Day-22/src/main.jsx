c73cc743e754ec8b19e4bb7d0e1fc2abc978b5b5
BanuPriya1392 <banusangee@gmail.com>
2026-01-13 11:16:01 +0530

    updating code 22

--- Day-22/src/main.jsx
+++ Day-22/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import NewsApp from "./App.jsx";
+import NewsApp from "./NewsApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
